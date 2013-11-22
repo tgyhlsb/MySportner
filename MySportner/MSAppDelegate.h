d9f78c93d8ebe7978238516cf79db5809bc126a3
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-11-22 22:32:05 +0100

    Initial Commit

---
+++ MySportner/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  MySportner
+//
+//  Created by Tanguy Hélesbeux on 22/11/2013.
+//  Copyright (c) 2013 MySportner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySportner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySportner
+//
+//  Created by Tanguy Hélesbeux on 22/11/2013.
+//  Copyright (c) 2013 MySportner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}

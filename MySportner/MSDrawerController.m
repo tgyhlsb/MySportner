235513b8c5f2eb754376ca5c89871343ae6ac9ee
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-11-24 19:46:29 +0100

    drawer bug fix on centerVC switch

--- MySportner/MSDrawerController.m
+++ MySportner/MSDrawerController.m
@@ -64,13 +64,8 @@
             [self closeDrawerAnimated:YES completion:nil];
         }
         else
-        {
-            double delayInSeconds = 0.3;
-            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-                [weakNavVC setViewControllers:@[newCenterVC]];
-            });
-            
+        {            
+            [weakNavVC setViewControllers:@[newCenterVC]];
             [self setCenterViewController:self.centerViewController withFullCloseAnimation:YES completion:^(BOOL finished) {
             }];
         }

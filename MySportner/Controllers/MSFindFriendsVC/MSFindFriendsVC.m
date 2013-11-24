4279118b814f012c717c30c6163d89bfe29f16d1
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-11-24 19:32:06 +0100

    ChooseSportcVC

---
+++ MySportner/Controllers/MSFindFriendsVC/MSFindFriendsVC.m
@@ -0,0 +1,38 @@
+//
+//  MSFindFriendsVC.m
+//  MySportner
+//
+//  Created by Tanguy Hélesbeux on 24/11/2013.
+//  Copyright (c) 2013 MySportner. All rights reserved.
+//
+
+#import "MSFindFriendsVC.h"
+
+@interface MSFindFriendsVC ()
+
+@end
+
+@implementation MSFindFriendsVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

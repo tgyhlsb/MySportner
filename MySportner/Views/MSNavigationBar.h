c6d0562c70d11529bf6e9d8691106ea26db8659f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-11-28 10:13:11 +0100

    ProfileVC + blur navigationBar

---
+++ MySportner/Views/MSNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  MSNavigationBar.h
+//  MySportner
+//
+//  Created by Tanguy Hélesbeux on 28/11/2013.
+//  Copyright (c) 2013 MySportner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSNavigationBar : UINavigationBar
+
+@end
---
+++ MySportner/Views/MSNavigationBar.m
@@ -0,0 +1,31 @@
+//
+//  MSNavigationBar.m
+//  MySportner
+//
+//  Created by Tanguy Hélesbeux on 28/11/2013.
+//  Copyright (c) 2013 MySportner. All rights reserved.
+//
+
+#import "MSNavigationBar.h"
+
+@implementation MSNavigationBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end

0c3dca4c426d324fa598f207d28af6c1e96c9805
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-12-01 12:55:09 +0100

    Profile and create account Appearance

---
+++ MySportner/MSFontFactory.h
@@ -0,0 +1,13 @@
+//
+//  MSFontFactory.h
+//  MySportner
+//
+//  Created by Tanguy Hélesbeux on 01/12/2013.
+//  Copyright (c) 2013 MySportner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSFontFactory : NSObject
+
+@end
---
+++ MySportner/MSFontFactory.m
@@ -0,0 +1,13 @@
+//
+//  MSFontFactory.m
+//  MySportner
+//
+//  Created by Tanguy Hélesbeux on 01/12/2013.
+//  Copyright (c) 2013 MySportner. All rights reserved.
+//
+
+#import "MSFontFactory.h"
+
+@implementation MSFontFactory
+
+@end

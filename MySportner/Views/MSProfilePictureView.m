57fc878a42c79000b2f505d17270feea2d360fb5
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-04-07 01:02:02 +0200

    Changed loading indicator on profile picture view

--- MySportner/Views/MSProfilePictureView.m
+++ MySportner/Views/MSProfilePictureView.m
@@ -32,7 +32,7 @@
 - (UIActivityIndicatorView *)activityIndicatorView
 {
     if (!_activityIndicatorView) {
-        _activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        _activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
         _activityIndicatorView.color = [UIColor blackColor];
         _activityIndicatorView.frame = self.bounds;
         _activityIndicatorView.hidesWhenStopped = YES;
